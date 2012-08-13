d297cf79af0fbfa993e036a3ff0da0286428af9d
Brian Flad <bflad417@gmail.com>
2012-08-13 17:59:04 -0400

    Temporarily handling chef_gem "pg" for COOK-1404

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,23 +17,26 @@
 # limitations under the License.
 #
 
-#if node[:stash][:database] == "postgresql" && node[:stash][:database_host] == "localhost"  
-#  include_recipe "database"
-#  database_connection = {:host => "#{node[:stash][:database_host]}", :port => node[:stash][:database_port], :username => 'postgres', :password => node[:postgresql][:password][:postgres]}
-#  database_info = Chef::EncryptedDataBagItem.load("stash","stash")
-#
-#  postgresql_database database_info[node.chef_environment]["database"] do
-#    connection database_connection
-#    action :create
-#  end
-#
-#  postgresql_database_user database_info[node.chef_environment]["database_user"] do
-#    connection database_connection
-#    password database_info[node.chef_environment]["database_password"]
-#    database_name database_info[node.chef_environment]["database"]
-#    action [:create, :grant]
-#  end
-#end
+if node[:stash][:database] == "postgresql" && node[:stash][:database_host] == "localhost"  
+  # Temporary handling of pg for COOK-1406
+  chef_gem "pg"
+  
+  include_recipe "database"
+  database_connection = {:host => "#{node[:stash][:database_host]}", :port => node[:stash][:database_port], :username => 'postgres', :password => node[:postgresql][:password][:postgres]}
+  database_info = Chef::EncryptedDataBagItem.load("stash","stash")
+
+  postgresql_database database_info[node.chef_environment]["database"] do
+    connection database_connection
+    action :create
+  end
+
+  postgresql_database_user database_info[node.chef_environment]["database_user"] do
+    connection database_connection
+    password database_info[node.chef_environment]["database_password"]
+    database_name database_info[node.chef_environment]["database"]
+    action [:create, :grant]
+  end
+end
 
 user node[:stash][:run_user] do
   comment "Stash Service Account"
