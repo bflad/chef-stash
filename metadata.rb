314bab45d0cd0e196db4edbf15aca22b8544cca9
Brian Flad <bflad417@gmail.com>
2012-08-14 10:42:16 -0400

    Added mysql cookbook dependency.

--- metadata.rb
+++ metadata.rb
@@ -10,6 +10,6 @@ recipe            "stash", "Installs Atlassian Stash"
   supports os
 end
 
-%w{ database git java postgresql }.each do |cb|
+%w{ database git java mysql postgresql }.each do |cb|
   depends cb
 end
