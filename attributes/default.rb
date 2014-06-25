b4cdbd8836f2a5891021c39e0b9bbd20edab7bf2
Lincoln Lee <git@lincoln.hk>
2014-06-25 13:55:14 +0800

    Update stash to 3.1.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,7 @@ default['stash']['install_type'] = 'standalone'
 default['stash']['service_type'] = 'init'
 default['stash']['url_base']     = 'http://www.atlassian.com/software/stash/downloads/binary/atlassian-stash'
 default['stash']['user']         = 'stash'
-default['stash']['version']      = '3.0.5'
+default['stash']['version']      = '3.1.0'
 
 default['stash']['url']      = "#{node['stash']['url_base']}-#{node['stash']['version']}.tar.gz"
 default['stash']['checksum'] =
@@ -70,6 +70,7 @@ when '2.12.4' then 'fc9c50f9aee43b677fe61921823137525675d66962583c53e4384400abee
 when '3.0.1' then '53312a7d26e68b50eb778b8847e04162074b645c2d4014bc404eba59ff90f624'
 when '3.0.4' then '2dc4db2a0fd306bad39d46b9e6a58d7c9d82c73711e002a93103f19e8133aa6a'
 when '3.0.5' then 'f06b4d545d0e7acae6255f3f4d8c9a02bbdfebf16da793eaef8e1b8eef9303b2'
+when '3.1.0' then '9eb7ba9eec7b67cf14a3ae450c8ec3d8e9289a120672825ec8ba6a290b48c6a6'
 end
 
 default['stash']['apache2']['access_log']         = ''
