fdc47ed03a687006d1536ba28afc7d4960da8ccb
Mikhail Zholobov <mzholobov@parallels.com>
2015-02-05 16:35:47 +0200

    Changed behavior for backup via cron
    Implemented cron job for 'backup_client' and 'backup_diy' as well.
    recipe 'backup_client_cron' is marked as deprecated

--- recipes/backup_client_cron.rb
+++ recipes/backup_client_cron.rb
@@ -1,5 +1,12 @@
 Stash.check_for_old_attributes!(node)
 
+Chef::Log.warn <<-EOH
+Recipe 'stash::backup_client_cron' is deprecated. Please, set attrite
+`node['stash']['backup']['crone']['enable'] = true` to configure crond to run
+Stash backup periodically. This recipe will be removed in the next major release
+of the 'stash' cookbook.
+EOH
+
 settings = Stash.settings(node)
 
 cron_d 'atlassian-stash-backup-client' do
@@ -10,4 +17,5 @@ cron_d 'atlassian-stash-backup-client' do
   weekday settings['backup']['cron']['weekday']
   command "java -jar #{node['stash']['backup_client']['install_path']}/stash-backup-client/stash-backup-client.jar"
   user node['stash']['user']
+  action(settings['backup']['cron']['enable'] ? :create : :delete)
 end
