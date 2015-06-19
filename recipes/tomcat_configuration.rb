6a4842ee0b3cfbf90e0994f22cd9be57914a1c1c
Lincoln Lee <git@lincoln.hk>
2015-06-19 22:47:40 +0800

    Use new server.xml and move the shared folder
    https://confluence.atlassian.com/display/STASH/Stash+upgrade+guide#Stashupgradeguide-Stash3.8updatenotes

--- recipes/tomcat_configuration.rb
+++ recipes/tomcat_configuration.rb
@@ -1,5 +1,10 @@
 settings = Stash.settings(node)
 stash_version = Chef::Version.new(node['stash']['version'])
+server_xml_path = "#{node['stash']['install_path']}/stash/conf/server.xml"
+
+if stash_version >= Chef::Version.new('3.8.0')
+  server_xml_path = "#{node['stash']['home_path']}/shared/server.xml"
+end
 
 template "#{node['stash']['install_path']}/stash/bin/setenv.sh" do
   if stash_version < Chef::Version.new('3.8.0')
@@ -12,13 +17,15 @@ template "#{node['stash']['install_path']}/stash/bin/setenv.sh" do
   notifies :restart, 'service[stash]', :delayed
 end
 
-template "#{node['stash']['install_path']}/stash/conf/server.xml" do
+template server_xml_path do
   if stash_version.major == 1
     source 'server.xml.erb'
-  elsif stash_version >= Chef::Version.new('3.3.0')
+  elsif stash_version < Chef::Version.new('3.3.0')
+    source 'server-tomcat7.xml.erb'
+  elsif stash_version < Chef::Version.new('3.8.0')
     source 'server-tomcat8.xml.erb'
   else
-    source 'server-tomcat7.xml.erb'
+    source '3.8+/server.xml.erb'
   end
   owner node['stash']['user']
   mode '0640'
