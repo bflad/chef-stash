405c8eba72e6b8619405b6341590bfef7ce07d80
Patrick Connolly <patrick.c.connolly@gmail.com>
2015-07-07 14:59:42 -0400

    Revert back to using libary for setting default mysql port.

--- attributes/default.rb
+++ attributes/default.rb
@@ -187,11 +187,13 @@ default['stash']['database']['type']     = 'mysql'
 default['stash']['database']['version']  = '5.6'
 
 default['stash']['database']['host']     = '127.0.0.1'
-default['stash']['database']['port']     = '3306'
 default['stash']['database']['name']     = 'stash'
 default['stash']['database']['password'] = 'changeit'
 default['stash']['database']['testInterval'] = 2
 default['stash']['database']['user']     = 'stash'
+
+# See `libraries/stash.rb` for code to set actual default port
+default['stash']['database']['port']     =  nil
 
 default['stash']['jvm']['minimum_memory']  = '512m'
 default['stash']['jvm']['maximum_memory']  = '768m'
