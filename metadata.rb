198235c909f7947d3273506e2066fd02faf725c5
Lincoln Lee <git@lincoln.hk>
2017-06-13 23:09:30 +0800

    Fix FC069

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@
 name 'stash'
 maintainer 'Brian Flad'
 maintainer_email 'bflad417@gmail.com'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs/Configures Atlassian Stash'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '4.5.1'
